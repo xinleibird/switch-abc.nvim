a434b6c7c2ce607fbde04bc636746ad91bf49cf0
辛磊 <xinleibird@gmail.com>
2026-03-20 00:59:06 +0800

    perf: skip keyboard switch when already on target layout

--- src/main.swift
+++ src/main.swift
@@ -4,6 +4,15 @@ import Carbon
 let arguments = CommandLine.arguments
 let targetID = arguments.count > 1 ? arguments[1] : "com.apple.keylayout.ABC"
 
+if let currentSource = TISCopyCurrentKeyboardInputSource()?.takeRetainedValue() {
+    if let pointer = TISGetInputSourceProperty(currentSource, kTISPropertyInputSourceID) {
+        let currentID = Unmanaged<CFString>.fromOpaque(pointer).takeUnretainedValue() as String
+        if currentID == targetID {
+            exit(0)
+        }
+    }
+}
+
 let filter = [kTISPropertyInputSourceID: targetID] as CFDictionary
 let sources = TISCreateInputSourceList(filter, false).takeRetainedValue() as! [TISInputSource]
 
